dd68fc5a679bc8d23ce50caec37dabc38924c5ad
Jana Dostálová <dostalova2jana@gmail.com>
2020-12-10 19:03:28 +0100

    commit 2

--- .eleventy.js
+++ .eleventy.js
@@ -6,7 +6,8 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("images");
 
 	// Zkopírovat `css/` to `_site/css/`
-	eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("js");
 
   return {
     // možné formáty šablon
